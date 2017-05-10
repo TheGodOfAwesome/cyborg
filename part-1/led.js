c0c1e55d0e800471d962501d49edc180cb68d202
Alan Hortz <alan.hortz@gmail.com>
2017-05-10 22:11:31 +0200

    stdout the device address

--- part-1/led.js
+++ part-1/led.js
@@ -9,12 +9,12 @@ We will use the node-metawear library to connect to the sensor and light the led
 var metawear = require('node-metawear');
 
 metawear.discover(function(device) {
-  console.log('metawear discovered');
+  console.log('metawear discovered', device.address);
 
   device.connectAndSetup(function(error) {
     console.log('metawear connected');
     var led = new device.Led(device);
-    led.config.setColor(led.config.GREEN);
+    led.config.setColor(led.config.BLUE);
     led.config.patternSolid();
     led.commitConfig();
     led.play();
